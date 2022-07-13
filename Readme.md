d1965780f44f2ee9bde4fef9f8ca1dccd1caf128
Douglas A B Novato <douglasabnovato@gmail.com>
2022-07-13 16:31:18 -0300

    iniciado projeto web de quiz app

---
+++ Readme.md
@@ -0,0 +1,65 @@
+<h3 align="center">
+  Quiz App 🚀
+</h3>
+
+<p align="center" style="display: flex; align-items: flex-start; justify-content: center;">
+  <img alt="versão 1.0 do projeto - Home" title="#quiz-app" src="./.github/tela-1.jpg">
+</p>  
+
+---
+
+### Sobre projeto 
+
+Um quiz que consome as questões da api Open Trivia DB API. Salva os ponto no Local Storage.
+Utiliza uma progress bar para as perguntas, uma spinning loader icon construída manualmente, os elementos do HTML gerados dinamicamente com JS.
+
+<p align="center" style="display: flex; align-items: flex-start; justify-content: center;">
+  <img alt="versão 1.0 do projeto - Home" title="#quiz-app" src="./.github/tela-1.jpg" width="400px">
+  <img alt="versão 1.0 do projeto - Loading" title="#quiz-app" src="./.github/tela-2.jpg" width="400px">
+  <img alt="versão 1.0 do projeto - Correct Answer" title="#quiz-app" src="./.github/tela-3.jpg" width="400px">
+  <img alt="versão 1.0 do projeto - Wrong Answer" title="#quiz-app" src="./.github/tela-4.jpg" width="400px">
+  <img alt="versão 1.0 do projeto - The End" title="#quiz-app" src="./.github/tela-5.jpg" width="400px">
+  <img alt="versão 1.0 do projeto - High Scores" title="#quiz-app" src="./.github/tela-6.jpg" width="400px">
+</p> 
+
+#### Tecnologias / Detalhes
+
+- JS
+- HTML/CSS
+- Git e GitHub 
+- VSCode
+- [Fonte do projeto: James Quick - Build a Quiz App with HTML CSS Javascript - Udemy](https://www.udemy.com/course/build-a-quiz-app-with-html-css-and-javascript/learn/lecture/13703646#overview) 
+
+#### Passo a passo do projeto 
+
+- [x] 1. Introduction and Resources. 
+- [x] 2. Create and Style the home page .
+- [x] 3. Extensions: Live Server 5.6.1 : in `index.html`, click in the `Open with Live Server`, então `http://127.0.0.1:5500/index.html`
+- [x] 3.b. CSS-tricks: `rem` for global sizing and `rem` for local sizing.
+- [x] 4. Create and style the game page.
+- [x] 5.1. Display hard coded questions and answers.
+- [x] 5.2. Display feedback for Correct / Incorrect Answers.
+- [x] 6. Create Head's up display (HUD).
+- [x] 7. Create a progress bar.
+- [x] 8. Create and style The End page.
+- [x] 9. Save High Scores in Local Storage. 
+- [x] 10. Load and Display High Scores from Local Storage. 
+- [x] 11. Fetch API to Load Questions from `Local Json file`.
+- [x] 12.1.  Fetch API to Load Questions from `Open Trivia DB API`
+- [x] 12.2.  fetch: 50, history, easy, multiple choice, default encoding: `https://opentdb.com/api.php?amount=50&category=23&difficulty=easy&type=multiple`
+- [x] 12.3.  json: 50, general knowledge, easy, multiple choice, default encoding: `https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple`
+- [x] 12.4.  json: 50, animals, easy, multiple choice, default encoding: `https://opentdb.com/api.php?amount=50&category=27&difficulty=easy&type=multiple`
+- [x] 12.5.  json: 50, science computers, easy, multiple choice, default encoding: `https://opentdb.com/api.php?amount=50&category=18&difficulty=easy&type=multiple`
+- [x] 12.6. `https://opentdb.com/`
+- [x] 13. Create a Spinning Loader. 
+- [x] 14. Closing
+
+#### Próximo passo
+
+- [ ] Responsividade
+- [ ] Incorporar questões via json
+- [ ] Mais de um jogador simultâneo
+
+---
+
+.: Por [Douglas A B Novato](https://linktr.ee/douglasabnovato) 
---
+++ src/javascript/game.js
@@ -0,0 +1,111 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+/* Local JSON file : questions.json */
+let questions = [];
+
+/* Local TRIVIA : url */
+fetch(
+  "https://opentdb.com/api.php?amount=50&category=23&difficulty=easy&type=multiple"
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formattedQuestion = {
+        question: loadedQuestion.question,
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+
+    startGame();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 30;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score); /*go to the end page*/
+    return window.location.assign("../pages/end.html");
+  }
+
+  questionCounter++;
+  progressText.innerHTML = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`; //update the progress bar
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+
+    const selectedChoice = e.target;
+    const selectAnswer = selectedChoice.dataset["number"];
+    const classToApply =
+      selectAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerHTML = score;
+};
