740e13cbb935dd9770f6214e0f3ceaff45d07a79
Douglas A B Novato <douglasabnovato@gmail.com>
2022-08-11 07:37:20 -0300

    detalhes e referências para construir aperfeiçoamentos

--- src/javascript/game.js
+++ src/javascript/game.js
@@ -12,10 +12,9 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
 let questions = [];
 
-fetch("./../json/questions.json")
+fetch("./../data/questions.json")
   .then((res) => {
     return res.json();
   })
@@ -25,24 +24,19 @@ fetch("./../json/questions.json")
         question: loadedQuestion.question,
         category: loadedQuestion.category,
       };
-
       const answerChoices = [...loadedQuestion.incorrect_answers];
 
       formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-
       answerChoices.splice(
         formattedQuestion.answer - 1,
         0,
         loadedQuestion.correct_answer
       );
-
       answerChoices.forEach((choice, index) => {
         formattedQuestion["choice" + (index + 1)] = choice;
       });
-
       return formattedQuestion;
     });
-
     startGame();
   })
   .catch((err) => {
@@ -50,7 +44,7 @@ fetch("./../json/questions.json")
   });
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 30;
+const MAX_QUESTIONS = 30; 
 
 startGame = () => {
   questionCounter = 0;
@@ -66,21 +60,18 @@ getNewQuestion = () => {
     localStorage.setItem("mostRecentScore", score); /*go to the end page*/
     return window.location.assign("../pages/end.html");
   }
-
   questionCounter++;
   progressText.innerHTML = `Question ${questionCounter}/${MAX_QUESTIONS}`;
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`; //update the progress bar
-
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
   category.innerText = currentQuestion.category;
-
   choices.forEach((choice) => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
-
   availableQuestions.splice(questionIndex, 1);
   acceptingAnswers = true;
 };
@@ -89,7 +80,6 @@ choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     if (!acceptingAnswers) return;
     acceptingAnswers = false;
-
     const selectedChoice = e.target;
     const selectAnswer = selectedChoice.dataset["number"];
     const classToApply =
@@ -98,13 +88,11 @@ choices.forEach((choice) => {
     if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
     }
-
     selectedChoice.parentElement.classList.add(classToApply);
-
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
-    }, 1000);
+    }, 1500);
   });
 });
 
