4dc97192a03e7e029d6c4186a24b765a9495ab52
Douglas A B Novato <douglasabnovato@gmail.com>
2022-08-12 14:41:21 -0300

    Ajustando requisição das questões do file js

--- src/data/questions.js
+++ src/data/questions.js
@@ -61,19 +61,5 @@ let questions = [
       answer: "Palmeiras",
       options: ["Flamengo", "São Paulo", "Palmeiras", "Santos"],
     },
-    // you can uncomment the below codes and make duplicate as more as you want to add question
-    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  
-    //   {
-    //   numb: 6,
-    //   question: "Your Question is Here",
-    //   answer: "Correct answer of the question is here",
-    //   options: [
-    //     "Option 1",
-    //     "option 2",
-    //     "option 3",
-    //     "option 4"
-    //   ]
-    // },
   ];
   
